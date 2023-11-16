0d3cc1d5632274847973f776ddca50a580f1c401
Riley_Marshall <rileyrmarshall1@gmail.com>
2023-11-16 09:07:40 -0700

    Initial Commit

---
+++ gitHubPractive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitHubPractive
+//
+//  Created by Riley Marshall on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitHubPractive/gitHubPractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubPractiveApp.swift
+//  gitHubPractive
+//
+//  Created by Riley Marshall on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct gitHubPractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
